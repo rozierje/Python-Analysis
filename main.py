d136fd6edc0a1bd5e63a33fa3730d4cc682d4b2f
rozierje <79817290+rozierje@users.noreply.github.com>
2021-04-09 01:18:31 -0400

    Add files via upload
    For Bank Analysis

--- main.py
+++ main.py
@@ -1,90 +1,69 @@
-# import operating system
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
 import os
 
-# impoert the csv reader
+# Module for reading CSV files
 import csv
-#locate csv file for election results
-csvpath = os.path.join('..', 'PyPoll', 'Resources', 'election_data.csv')
-#create empty carts for candidate list
-candidate_list = []
-#static adding candidates
-candidates = {}
-candidatename = ""
-#dynamic adding candidates
-#candidates["Khan"] = 0
-winnervotes = 0
-winner = ""
-#set vote count total
-total_votes = 0
-
+#open file
+csvpath = os.path.join('..', 'PyBank', 'Resources', 'budget_data.csv')
+#declare variables
+month = 0
+change = 0
+month_name = ""
+total_months = 0
+total_period = 0
+greatest_increase = 0
+increase_month = ""
+greatest_decrease = 0
+decrease_month = ""
+decrease = 0
+increase = 0
+avg_change = 0
+#declare dictionaries
+profit = {}
+loss = {}
 # Format Final Table
-print("Election Results")
+print("Financial Analysis")
+
 print("-------------------")
 
-#Gather Total Votes
-with open(csvpath) as election_results:
-   # CSV reader specifies delimiter and variable that holds contents
-   csvreader = csv.reader(election_results, delimiter=',')
-   # reading first row of the file
-   header = next(csvreader)
-   # Read each additional row of data after the header
-   for row in csvreader:
-      #building total votes
-      total_votes += 1
-      #getting candidate name from column index 2
-      candidatename = row[2]
-         #searching for candidate name in candidate_list
-      if candidatename not in candidate_list:
-      # if not in list, add it; if in list move on
-         candidate_list.append(candidatename)
-         #if already in list, add the candidate to the dictionary; 
-         # initialize candidate with 1 vote
-         #{"Khan":0}
-         candidates[candidatename] = 0
-      candidates[candidatename] = candidates[candidatename] + 1
-   print("Total Votes")
-   print(total_votes)
-   print("-----------------------")
-#-------------------- good to this point------------------------------        
-      #finding the individual candiate names
-   for candidate in candidates:
-      #for each candidate, get the votes for that candidate
-      candidate_votes = candidates[candidate]
-      #for total per candidate, divide by total votes cast
-      candidate_percentage = candidate_votes/total_votes * 100
-      #print the strings for formatting
-      print(f"{candidate}: {candidate_percentage:.3f} ({candidate_votes})")
-      #going through dictionary to find candidate with most votes (line 51)
-      if candidate_votes > winnervotes:
-         #assigning candidate_votes to winner votes
-         winnervotes = candidate_votes
-         #assigns candidate to winner
-         winner = candidate
-   print("--------------------------------")
-   print(f"Winner: {winner}")
-   print("--------------------------------")
+with open(csvpath) as csvfile:
 
-
-################### write new test file ########################
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #skip header
+    csv_header = next(csvfile)
+    # Read each row of data after the header
+    for row in csvreader:
+        total_months += 1
+        total_period = total_period + int(row[1])
+    
+        #profit_loss.append(change)
+        
+    print ("Total Months: " + str(total_months) + "")
+    print(("Total: $" + str(total_period) + ""))
+    print("Average Change: $" + str(avg_change) + "")
+    print(f"Greatest Increase in Profits: {increase_month} ${greatest_increase}")
+    print (f"Greated Decrease in Profits: {decrease_month} ${greatest_decrease}")
 
 # Specify the file to write to
-output_path = os.path.join("..", "PyPoll", "Resources", "results.csv")
+output_path = os.path.join("..", "PyBank", "Analysis", "analysis.csv")
 
 # Open the file using "write" mode. Specify the variable to hold the contents
-with open(output_path, 'w', newline='') as csvfile:
+with open(output_path, 'w', newline='') as txtfile:
 
-   # Initialize csv.writer
-   csvwriter = csv.writer(csvfile, delimiter=',')
+    # Initialize csv.writer
+    csvwriter = csv.writer(txtfile, delimiter=',')
 
-   # Write the first row (column headers)
-   csvwriter.writerow(["Election Results"])
-   csvwriter.writerow(["-------------------------------------"])
-   # Write the second row
-   csvwriter.writerow(["Total Votes: " + str(total_votes) +" "])
-   csvwriter.writerow(["--------------------------------------"])
-   csvwriter.writerow([(f"{candidates}: ")])
-   csvwriter.writerow(["--------------------------------------"])
-   csvwriter.writerow([f"Winner: {winner}"])
-   csvwriter.writerow(["--------------------------------------"])
+    csvwriter.writerow(["Financial Analysis"])
+    
+    csvwriter.writerow(["-------------------------------------"])
+    csvwriter.writerow(["Total Months: " + str(total_months) +""])
+    csvwriter.writerow([f"Total: ${total_period}"])
+    csvwriter.writerow([f"Average Change: ${avg_change}"])
+    csvwriter.writerow([f"Greatest Increase in Profits: {increase_month} ${greatest_increase}"])
+    csvwriter.writerow([f"Greatest Decrease in Profits: {decrease_month} ${greatest_decrease}"])
 
-print("Election Results are In!")
+    csvwriter.writerow(["--------------------------------------"])
+  
+#-------------------------unsure if this code runs---------------------
